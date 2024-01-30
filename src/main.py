fac67be3b839c9155f9a3789fb9498305822392a
khot-aditya <adityakhot6838@gmail.com>
2024-01-30 22:20:47 +0530

    updates

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import json
 from appwrite.client import Client
 import os
-from skpy import Skype
+from skpy import Skype, SkypeAuthException
 
 # This is your Appwrite function
 # It's executed each time we get a request
@@ -14,26 +14,38 @@ def main(context):
         return context.res.send("Hello, World!")
     
     if context.req.method == "POST":
+        try:
+            context.log(context.req)
+            # Access POST data from context.req.data
+            post_data = context.req.body
+            username = post_data.get("username")
+            password = post_data.get("password")
+            chatId = post_data.get("chatId")
+            message = post_data.get("message")
+            
+            # Create a Skype connection
+            sk = Skype(username, password)
+
+            contact_username = chatId
+            contact = sk.contacts[contact_username]
+
+            # Send a text message
+            message_content = message
+            contact.chat.sendMsg(message_content)
+
+            return context.res.send("Message sent successfully.")
         
-        context.log(context.req)
-        # Access POST data from context.req.data
-        post_data = context.req.body
-        username = post_data.get("username")
-        password = post_data.get("password")
-        chatId = post_data.get("chatId")
-        message = post_data.get("message")
+        except SkypeAuthException as e:
+            return context.res.send(f"Skype authentication failure: {e}")
         
-        # Create a Skype connection
-        sk = Skype(username, password)
-
-        contact_username = chatId
-        contact = sk.contacts[contact_username]
-
-        # Send a text message
-        message_content = message
-        contact.chat.sendMsg(message_content)
-
-        return context.res.send("Message sent successfully.")
+        except Exception as e:
+            return context.res.send(f"Error in Skype connection: {e}")
+        
+        finally:
+            # Close the Skype connection to ensure proper cleanup
+            if 'sk' in locals():
+                sk.conn.close()
+                context.log("Skype connection closed.")
 
     # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
